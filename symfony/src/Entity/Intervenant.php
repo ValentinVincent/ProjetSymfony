8962dd92738588dabbc1e35c63ccfe7c1c2d8089
Valentin Vincent <valentinhov95@gmail.com>
2020-01-08 14:06:21 +0100

    database ready

---
+++ symfony/src/Entity/Intervenant.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\IntervenantRepository")
+ */
+class Intervenant
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $age;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\matiere", mappedBy="intervenant")
+     */
+    private $matiere;
+
+    public function __construct()
+    {
+        $this->matiere = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getAge(): ?string
+    {
+        return $this->age;
+    }
+
+    public function setAge(?string $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|matiere[]
+     */
+    public function getMatiere(): Collection
+    {
+        return $this->matiere;
+    }
+
+    public function addMatiere(matiere $matiere): self
+    {
+        if (!$this->matiere->contains($matiere)) {
+            $this->matiere[] = $matiere;
+            $matiere->setIntervenant($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMatiere(matiere $matiere): self
+    {
+        if ($this->matiere->contains($matiere)) {
+            $this->matiere->removeElement($matiere);
+            // set the owning side to null (unless already changed)
+            if ($matiere->getIntervenant() === $this) {
+                $matiere->setIntervenant(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ symfony/src/Entity/Projet.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProjetRepository")
+ */
+class Projet
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbMax;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $note;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\matiere", inversedBy="projets")
+     */
+    private $matiere;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNbMax(): ?int
+    {
+        return $this->nbMax;
+    }
+
+    public function setNbMax(int $nbMax): self
+    {
+        $this->nbMax = $nbMax;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(?int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getMatiere(): ?matiere
+    {
+        return $this->matiere;
+    }
+
+    public function setMatiere(?matiere $matiere): self
+    {
+        $this->matiere = $matiere;
+
+        return $this;
+    }
+}
